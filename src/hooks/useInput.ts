d6314831c229186547862789f4bc7902fac7241a
jejecrunch <gkswlgp456@gmail.com>
2023-09-21 17:24:42 +0900

    Add hooks

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+type UseInputParam = {
+  initVal: string | "";
+  validation: (v: string) => boolean | never;
+};
+
+export default function useInput({ initVal, validation }: UseInputParam) {
+  const [value, setValue] = useState(initVal);
+  const [valid, setValid] = useState(false);
+
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const {
+      target: { value },
+    } = e;
+
+    setValue(value);
+    setValid(validation(value));
+  }
+
+  function onReset() {
+    setValue("");
+  }
+
+  return { value, onChange, valid, onReset };
+}
